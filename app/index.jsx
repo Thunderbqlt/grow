eb3a6b358cdfe825fb5bb13fe2dea1fd64baf4b0
Thunderbqlt <samw3363@gmail.com>
2024-07-30 12:28:15 +1200

    Started work on an about page and added tailwind css

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Welcome to Grow!</Text>
       <StatusBar style="auto" />
+      <Link href="/about" style={{ color: 'blue'}}>What is Grow about?</Link>
     </View>
-  );
+  );  
 }
 
 const styles = StyleSheet.create({
