552dcea876e190de9678e92f67be8fcdffc21af2
Thunderbqlt <samw3363@gmail.com>
2024-08-06 22:56:05 +1200

    Started work on about page

--- app/(tabs)/about.jsx
+++ app/(tabs)/about.jsx
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const About = () => {
   return (
-    <View>
-      <Text>About</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-black text-center font-psemibold">WHAT WE DO</Text>
+        <View className="rounded-xl bg-accent">
+        <Text className="text-md text-black text-center font-pregular">Grow is an app for you to keep a track of your garden. Grow is your pocket companion to manage any garden, from a sprawling community project to your single veggie plot!</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- app/(tabs)/plots.jsx
+++ app/(tabs)/plots.jsx
@@ -1,12 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Plots = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Plots</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
