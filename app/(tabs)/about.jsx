b73ad0b457cb87b62f2d0fac39943a809c448670
Thunderbqlt <samw3363@gmail.com>
2024-08-13 19:57:43 +1200

    About page complete + custom button component created

--- app/(tabs)/about.jsx
+++ app/(tabs)/about.jsx
@@ -1,31 +1,31 @@
 import { View, Text, ScrollView, Button, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { useRouter } from 'expo-router'
+import { Redirect, router, useRouter } from 'expo-router'
 import { images } from '../../constants';
+import CustomButton from '../../components/CustomButton';
 
 
 const About = () => {
-  const router = useRouter(); 
-const navigateToTab = (tabName) => {
-  router.push(tabName);     
-};
+  
   return (
     <SafeAreaView>
       <ScrollView className="px-4 my-6">
         <View className="w-full justify-center items-center h-full px-4">
         <Text className="text-2xl text-black text-center font-psemibold">WHAT WE DO</Text>
-        <View className="rounded-xl bg-accent">
+        <View className="rounded-xl bg-accent w-full mt-2">
         <Text className="text-md text-black text-center font-pregular">Grow is an app for you to keep a track of your garden. Grow is your pocket companion to manage any garden, from a sprawling community project to your single veggie plot!</Text>
-        <Button
-          title="START NOW"
-          onPress={() =>
-            navigateToTab('home')}
-        />
+        
         </View>
         <Image
           source={images.garden}
-          className="max-w-[380px] w-full h-[300px] rounded-xl"
+          className="max-w-[380px] w-full h-[300px] rounded-xl mt-6"
+        />
+
+        <CustomButton 
+          title="Get Started"
+          handlePress={() => router.push('/home')}
+          containerStyles="w-[200px] mt-7"
         />
 
         </View>
