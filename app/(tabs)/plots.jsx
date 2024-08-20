84ee1600e31888831e1b6d75a674c6e66fbb0e24
Thunderbqlt <samw3363@gmail.com>
2024-08-20 14:21:45 +1200

    created database with appwrite

--- app/(tabs)/plots.jsx
+++ app/(tabs)/plots.jsx
@@ -2,7 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+
 const Plots = () => {
+  getPlants()
+  async function getPlants() {
+    const res = await databases.listDocuments(config.databaseId, config.plotCollectionId);
+
+    console.log(res);
+  }
   return (
     <SafeAreaView>
       <Text>Plots</Text>
