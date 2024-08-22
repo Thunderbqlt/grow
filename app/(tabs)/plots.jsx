17f8694639f16ed33d9e04a187cc67aab8257999
Thunderbqlt <samw3363@gmail.com>
2024-08-23 10:27:55 +1200

    started to create code to display information from my database

--- app/(tabs)/plots.jsx
+++ app/(tabs)/plots.jsx
@@ -1,18 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState, useEffect } from 'react';
 
 
 const Plots = () => {
-  getPlants()
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    getSuggestions();
+  }, []);
+
   async function getPlants() {
     const res = await databases.listDocuments(config.databaseId, config.plotCollectionId);
 
-    console.log(res);
+    console.log(res); 
+    setSuggestions(res.documents);
   }
   return (
     <SafeAreaView>
       <Text>Plots</Text>
+      <FlatList
+        data={suggestions}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.plant}</Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
   )
 }
