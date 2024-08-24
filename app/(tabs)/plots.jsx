ef0ea549b52e773c66fa68cb15d7b7d21ce16a1d
Thunderbqlt <samw3363@gmail.com>
2024-08-24 19:08:21 +1200

    Database now displays documents and added a text input

--- app/(tabs)/plots.jsx
+++ app/(tabs)/plots.jsx
@@ -1,8 +1,8 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState, useEffect } from 'react';
-
+import { config, databases } from '../../lib/appwrite.js';
 
 const Plots = () => {
   const [suggestions, setSuggestions] = useState([]);
@@ -11,15 +11,29 @@ const Plots = () => {
     getSuggestions();
   }, []);
 
-  async function getPlants() {
+  async function getSuggestions() {
     const res = await databases.listDocuments(config.databaseId, config.plotCollectionId);
 
     console.log(res); 
     setSuggestions(res.documents);
   }
+
+  const [text, setText] = useState('');
+
+  const handleChangeText = (input) => {
+    setText(input);
+  };
+
   return (
     <SafeAreaView>
       <Text>Plots</Text>
+      <TextInput
+        placeholder='What is planted here?'
+        value={text}
+        onChangeText={handleChangeText}
+
+      />
+      <Text>You typed: {text}</Text>
       <FlatList
         data={suggestions}
         keyExtractor={(item) => item.$id}
