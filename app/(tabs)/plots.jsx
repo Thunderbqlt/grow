7321cd0b730d6b56a933d3d33b06fbf8018829d0
Thunderbqlt <samw3363@gmail.com>
2024-09-05 22:39:23 +1200

    Added a input for notes

--- app/(tabs)/plots.jsx
+++ app/(tabs)/plots.jsx
@@ -8,7 +8,7 @@ import { Picker } from '@react-native-picker/picker';
 import { useRouter } from 'expo-router';
 
 const Plots = () => {
-
+  //Sets the default selected plot to Plot 1
   const [selectedPlot, setSelectedPlot] = useState("66c123d8000ccb68ad94");
   useEffect(() => {
     if (selectedPlot) {
@@ -22,7 +22,7 @@ const Plots = () => {
   useEffect(() => {
     getSuggestions();
   }, []);
-
+  //Gathers the information for each plot
   async function getSuggestions() {
     const res = await databases.listDocuments(config.databaseId, selectedPlot);
 
@@ -31,20 +31,29 @@ const Plots = () => {
   }
 
   const [text, setText] = useState('');
-
+  const [notes, setNote] = useState('');
+  //Updates the plant name input to display the text being input
   const handleChangeText = (input) => {
     setText(input);
   };
-
+  //Updates the note input to display the text being input
+  const handleChangeNote = (input) => {
+    setNote(input);
+  };
+  //Handles adding the data input to the database by creating a new document
   const handleAddPress = async () => {
       await databases.createDocument(
         config.databaseId,
         selectedPlot,
         'unique()', 
-        { plant: text } 
+        { plant: text,
+          info: notes
+         } 
       );
       getSuggestions();
+      //Clears the input view after adding the data
       setText('');
+      setNote('');
     }
 
 
@@ -52,7 +61,7 @@ const Plots = () => {
 
   return (
     <SafeAreaView>
-        <Picker
+        <Picker //Allows you to select which plot you want to view
           selectedValue={selectedPlot}
           onValueChange={(itemValue, itemIndex) =>
             setSelectedPlot(itemValue)
@@ -61,27 +70,34 @@ const Plots = () => {
           <Picker.Item label="Plot 2" value="66cd0e6700146d302820" />
           <Picker.Item label="Plot 3" value="66cd0e870022891b18de" />
         </Picker>
-      <TextInput className="text-2xl text-black text-center font-psemibold"
+      <TextInput //Input of the new plants name
+        className="text-2xl text-black text-center font-psemibold"
         placeholder='What is planted here?'
         value={text}
         onChangeText={handleChangeText}
-        
-
+      />
+      <TextInput //Input for notes about the plant
+        className="text-2xl text-black text-center font-psemibold mt-3"
+        placeholder='Any extra notes about this plant?'
+        value={notes}
+        onChangeText={handleChangeNote}
       />
       <View className="justify-center items-center">
-      <CustomButton 
+      <CustomButton //Button which adds the input data to the database
           title="Add"
           handlePress={handleAddPress}
           containerStyles="w-[80px] h-[20px] mt-7 bg-secondary"
         />
       <Text>This plot contains:</Text>
       
-      <FlatList className="h-screen"
+      <FlatList //List to display the plots plant information
+        className="h-screen"
         data={suggestions}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
           <View className="mt-5">
             <Text>{item.plant}</Text>
+            <Text>{item.info}</Text>
           </View>
         )}
       />
