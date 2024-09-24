2d877dc8000550be387eafaf18840dc38e4f1a8b
Thunderbqlt <samw3363@gmail.com>
2024-09-24 22:47:09 +1200

    improved welcome page

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Image, ImageBackground } from 'react-native';
 import { Link } from 'expo-router';
+import { images } from '../constants';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Welcome to Grow!</Text>
+      <Image //Image of the Environmental Club gardens
+          source={images.growbanner}
+          className="max-w-[380px] w-full h-[250px] rounded-xl mt-6 ml-7"
+        />
+      <Text className="text-3xl font-pmedium">Welcome to Grow!</Text>
       <StatusBar style="auto" />  
-      <Link href="/home" style={{ color: 'green'}}>Start with Grow now!</Link>
+      <Link className="font-pregular" href="/home" style={{ color: 'green'}}>Start with Grow now!</Link>
+      <Image //Image of the Environmental Club gardens
+          source={images.gardenvolunteers}
+          className="max-w-[380px] w-full h-[290px] rounded-xl mt-6"
+        />
     </View>
   );  
 }
--- constants/images.js
+++ constants/images.js
@@ -6,5 +6,8 @@ import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
 import garden from "../assets/images/garden.jpg";
+import gardenvolunteers from "../assets/images/gardenvolunteers.jpg";
+import growlogo from "../assets/images/growlogo.png";
+import growbanner from "../assets/images/growbanner.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty, garden };
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, garden, gardenvolunteers, growlogo, growbanner };
